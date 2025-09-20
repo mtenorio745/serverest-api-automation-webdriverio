e0f560b9e10411448b8af6bd6c2599dc3054c474
Mateus Tenório <mateus.tenorio.ext@xcelis.com.br>
2025-09-20 11:51:59 -0300

    fix: Corrige permissões de execucao do relatório6

--- wdio.conf.js
+++ wdio.conf.js
@@ -43,17 +43,24 @@ exports.config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    //maxInstances: 10,
+    maxInstances: 5,
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
+        maxInstances: 1,
         browserName: 'chrome',
-        'wdio:options': {
-            headless: true,
-        },
+        'goog:chromeOptions': {
+            args: [
+                '--no-sandbox',
+                '--disable-infobars',
+                '--headless',
+                '--disable-gpu',
+                '--window-size=1440,735'
+            ],
+        }
     }],
 
     //
